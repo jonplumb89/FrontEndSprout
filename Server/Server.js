5e60ecb7d0ddd1417313f286f03e621262329b0d
jonplumb89 <jonplumb89@outlook.com>
2021-09-09 01:07:38 -0400

    updated server

--- Server/Server.js
+++ Server/Server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const routes = require('./routes');
+const port = process.env.PORT || 3500;
+const cors = require('cors');
+
+app.use(cors());
 
 app.use(routes);
 
-app.listen(3500, () => {
+app.listen(port, () => {
     console.log("Server Running");
 });
